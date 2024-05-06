9e69786424fccd0895703ab458858ed060655c78
Samarium99 <sarah.mo@csedge.org>
2024-05-06 00:23:06 -0400

    Add Xcode

---
+++ noticeme-app/noticeme-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  noticeme-app
+//
+//  Created by Student on 5/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ noticeme-app/noticeme-app/noticeme_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  noticeme_appApp.swift
+//  noticeme-app
+//
+//  Created by Student on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct noticeme_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
